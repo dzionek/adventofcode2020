3b68c2b15e7fec5b2f1244a45324eb582e8ff23c
dzionek <vburtoch@gmail.com>
2020-12-01 06:22:43 +0100

    Add Day 1

---
+++ 1.py
@@ -0,0 +1,10 @@
+file = open('inputs/1.txt')
+data = file.readlines()
+data_set = {int(element) for element in data}
+
+# Part A - O(n)
+print([a * (2020 - a) for a in data_set if 2020 - a in data_set][0])
+
+# Part B - O(n^2)
+print([a * b * (2020 - a - b) for a in data_set for b in data_set
+       if 2020 - a - b in data_set][0])
